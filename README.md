d5285629c0252c29097ecdef90742ecbf993a0b2
otema666 <otema000@gmail.com>
2024-02-14 13:03:11 +0100

    keyboard binds fixed, readme updated

--- README.md
+++ README.md
@@ -15,5 +15,37 @@ curl -s https://raw.githubusercontent.com/otema666/my-packages/main/installer.sh
 ```
 
 
-![otema666](https://github.com/otema666/my-packages/assets/126337147/a511043d-62bd-4d5e-ba37-a43070736dad)
+## Keyboard Shortcuts
 
+- **Full Screen Terminal**: <kbd>Windows</kbd> + <kbd>F</kbd>
+
+- **Close Active Window**: <kbd>Windows</kbd> + <kbd>W</kbd>
+
+- **Switch to Workspace 1**: <kbd>Super</kbd> + <kbd>1</kbd>.
+
+- **Switch to Workspace 2**: <kbd>Super</kbd> + <kbd>2</kbd>.
+
+- **Switch to Workspace 3**: <kbd>Super</kbd> + <kbd>3</kbd>.
+
+- **Switch to Workspace 4**: <kbd>Super</kbd> + <kbd>4</kbd>.
+
+- **Move to Next Workspace**: <kbd>Super</kbd> + <kbd>Tab</kbd>.
+
+- **Move to Previous Workspace**: <kbd>Super</kbd> + <kbd>Shift</kbd> + <kbd>Tab</kbd>.
+
+- **Move Window to Workspace 1**: <kbd>Super</kbd> + <kbd>Shift</kbd> + <kbd>1</kbd>.
+
+- **Move Window to Workspace 2**: <kbd>Super</kbd> + <kbd>Shift</kbd> + <kbd>2</kbd>.
+
+- **Move Window to Workspace 3**: <kbd>Super</kbd> + <kbd>Shift</kbd> + <kbd>3</kbd>.
+
+- **Move Window to Workspace 4**: <kbd>Super</kbd> + <kbd>Shift</kbd> + <kbd>4</kbd>.
+
+- **Brave Browser**: <kbd>Control</kbd> + <kbd>Alt</kbd> + <kbd>B</kbd>
+
+- **Firefox Browser**: <kbd>Control</kbd> + <kbd>Alt</kbd> + <kbd>F</kbd>
+
+<br>
+<br>
+
+![otema666](https://github.com/otema666/my-packages/assets/126337147/a511043d-62bd-4d5e-ba37-a43070736dad)
--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@
 # var
 path=$(pwd)
 prerequisites=("lolcat" "figlet")
-apt_packages=("bat" "neovim" "curl" "exa" "aircrack-ng" "net-tools" "gcc" "neofetch" "ruby" "ruby-bundler" "ruby-dev" "octave" "lolcat" "git" "gparted" "nodejs" "gnome-tweaks" "gnome-shell-extensions" "gnome-shell-extension-prefs" "keepassxc" "dbus-x11" "python3-pip" "tree" "baobab")
+apt_packages=("bat" "neovim" "curl" "exa" "aircrack-ng" "net-tools" "gcc" "neofetch" "ruby" "ruby-bundler" "ruby-dev" "xdotool" "octave" "lolcat" "git" "gparted" "nodejs" "gnome-tweaks" "gnome-shell-extensions" "gnome-shell-extension-prefs" "keepassxc" "dbus-x11" "python3-pip" "tree" "baobab")
 snap_packages=("code --classic" "gimp" "brave" "discord" "vlc")
 message=0
 dconf="https://raw.githubusercontent.com/otema666/my-packages/main/otema666.dconf"
@@ -89,29 +89,79 @@ create_new_profile() {
 
 # super + enter bind function
 bind_fulls_terminal() {
-  # Terminal (SUPER + ENTER)
   local terminal_path="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['$terminal_path']"
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$terminal_path name 'Full Screen Terminal'
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$terminal_path command 'gnome-terminal --full-screen'
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$terminal_path binding '<Super>Return'
-  print_message "yellow" "\t [+] Configurado Terminal Pantalla Completa: (WIN + ENTER)"
+  local brave_path="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+  local firefox_path="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+  
+  local keybindings="[ '$terminal_path', '$brave_path', '$firefox_path' ]"
+
+  gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$keybindings"
+
+  # Terminal (SUPER + ENTER)
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$terminal_path name 'Full Screen Terminal'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$terminal_path command 'gnome-terminal --full-screen'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$terminal_path binding '<Super>Return'
+  print_message "yellow" "\t [+] Configurado Terminal Pantalla Completa: (SUPER + ENTER)"
 
   # Brave (CTRL + ALT + B)
-  local brave_path="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['$brave_path']"
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$brave_path name 'Brave'
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$brave_path command 'brave'
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$brave_path binding '<Control><Alt>b'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$brave_path name 'Brave'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$brave_path command 'brave'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$brave_path binding '<Control><Alt>b'
   print_message "yellow" "\t [+] Configurado Brave Browser: (CTRL + ALT + B)"
 
   # Firefox  (CTRL + ALT + F)
-  local firefox_path="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['$firefox_path']"
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$firefox_path name 'Launch Firefox'
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$firefox_path command 'firefox'
-  sudo gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$firefox_path binding '<Control><Alt>f'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$firefox_path name 'Launch Firefox'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$firefox_path command 'firefox'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$firefox_path binding '<Control><Alt>f'
   print_message "yellow" "\t [+] Configurado Firefox Browser: (CTRL + ALT + F)"
+  
+  # Close current window (Super + W)
+  gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
+  print_message "yellow" "\t [+] Configurado cerrar la ventana activa: (Super + W)"
+
+  # Config workspaces
+  gsettings set org.gnome.mutter dynamic-workspaces false
+  gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
+
+  # Workspace 1 (Super + 1)
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+  print_message "yellow" "\t [+] Configurado cambiar Workspace 1: (Super + 1)"
+
+  # Workspace 2 (Super + 2)
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+  print_message "yellow" "\t [+] Configurado cambiar Workspace 2: (Super + 1)"
+
+  # Workspace 3 (Super + 3)
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+  print_message "yellow" "\t [+] Configurado cambiar Workspace 3: (Super + 1)"
+
+  # Workspace 4 (Super + 4)
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+  print_message "yellow" "\t [+] Configurado cambiar Workspace 4: (Super + 1)"
+
+  # Move to next workspace (Super + Tab)
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Tab']"
+  print_message "yellow" "\t [+] Configurado mover a siguiente Workspace: (Super + Tab)"
+
+  # Move to previous workspace (Super + Shift + Tab)
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super><Shift>Tab']"
+  print_message "yellow" "\t [+] Configurado mover a anterior Workspace: (Super + Shift + Tab)"
+
+  # Move window to workspace 1 (Super + Shift + 1)
+  gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Super><Shift>1']"
+  print_message "yellow" "\t [+] Configurado mover ventana a Workspace 1: (Super + Shift + 1)"
+
+  # Move window to workspace 2 (Super + Shift + 2)
+  gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Super><Shift>2']"
+  print_message "yellow" "\t [+] Configurado mover ventana a Workspace 2: (Super + Shift + 2)"
+
+  # Move window to workspace 3 (Super + Shift + 3)
+  gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Super><Shift>3']"
+  print_message "yellow" "\t [+] Configurado mover ventana a Workspace 3: (Super + Shift + 3)"
+
+  # Move window to workspace 4 (Super + Shift + 4)
+  gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Super><Shift>4']"
+  print_message "yellow" "\t [+] Configurado mover ventana a Workspace 4: (Super + Shift + 4)"
 }
 
 variables_personales (){
@@ -149,6 +199,10 @@ done
 
 clear && figlet "Auto installer" -c | lolcat
 
+print_message "cyan" "===== Actualizando repositorios ====="
+sudo apt update && sudo apt upgrade -y &> /dev/null
+print_message "cyan" "===== Sistema actualizado correctamente =====\n"
+
 if gsettings set org.gnome.desktop.interface color-scheme prefer-dark; then
   print_message "[+] Tema oscuro aplicado."
 else
@@ -168,14 +222,12 @@ else
   print_message "red" "\t[!] Error al configurar botón de actividades."
 fi
 
-
 # Extension
 print_message "cyan" "===== Configurando extensiones... ====="
 if gnome_extension; then
   print_message "green" "\t[+] Extensiones configuradas correctamente."
 else
   print_message "red" "\t[!] Error al configurar las extensiones."
-  # print_message "red" "\t[!] Error al configurar atajos del teclado."
 fi
 
 # Background
@@ -186,15 +238,9 @@ else
   print_message "red" "\t[!] Error al configurar el fondo de pantalla"
 fi
 
-# start with sudo
-print_message "cyan" "===== Actualizando repositorios ====="
-sudo apt update && sudo apt upgrade -y &> /dev/null
-print_message "cyan" "===== Sistema actualizado correctamente =====\n"
-
 print_message "cyan" "===== Instalando programas ====="
 
 # Apt
-
 for package in "${apt_packages[@]}"; do
     if dpkg-query -W -f='${Status}' "$package" 2>/dev/null | grep -q "install ok installed"; then
         print_message "yellow" "\t[-] $package ya estaba instalado."
@@ -289,11 +335,10 @@ fi
 
 # Atajos de teclado
 print_message "cyan" "===== Configurando atajos de teclado... ====="
-if false && bind_fulls_terminal; then
+if bind_fulls_terminal; then
   print_message "green" "\t[+] Atajos configurados correctamente."
 else
-  print_message "red" "\t[!] Esta funcion todavia no esta disponible."
-  # print_message "red" "\t[!] Error al configurar atajos del teclado."
+  print_message "red" "\t[!] Error al configurar atajos del teclado."
 fi
 
 print_message "cyan" "===== Configurando variables personales... ====="
@@ -301,6 +346,10 @@ variables_personales
 
 print_message "cyan" "===== Actualizando repositorios ====="
 sudo apt update &> /dev/null
-print_message "cyan" "===== Sistema actualizado correctamente ====="
+print_message "cyan" "===== Sistema actualizado correctamente =====\n"
+
+print_message "red" "Es necesario cerrar sesión para aplicar la configuración"
+sleep 3
+gnome-session-quit
 
 exit 0
